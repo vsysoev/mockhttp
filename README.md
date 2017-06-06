486ea311e93505b15f72d7f003e0c0914c47d2d8
Sysoev, Vladimir <vladimir.sysoev@sarov-itc.ru>
2017-06-06 14:36:14 +0300

    implemented simple mock server

---
+++ README.md
@@ -0,0 +1,13 @@
+#mockhttp
+Simple http mock server for testing purpose. Need to mock backend for simulating
+particular behavior.
+Behavior is defined in yml config file. Sample is
+```yaml
+/hello:                           // entrypoint
+  status: 200                     // status code
+  delay: 200ms                    // delay in time.Duration
+  body: '{"status":"OK"}'         // body as string
+/world:
+  status: 404
+  delay: 1s
+```
---
+++ mockhttp.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"flag"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+
+	yaml "gopkg.in/yaml.v2"
+)
+
+type (
+	// Reply stores reply for entrypoint
+	Reply struct {
+		Status int
+		Body   string
+		Delay  time.Duration
+	}
+)
+
+func main() {
+	addr := flag.String("url", ":8080", "URL to listen to")
+	config := flag.String("config", "./mockhttp.yml", "Config file")
+	flag.Parse()
+	content, e := ioutil.ReadFile(*config)
+	if e != nil {
+		log.Fatal(e)
+	}
+	ymlCfg := make(map[string]Reply, 0)
+	e = yaml.Unmarshal(content, &ymlCfg)
+	if e != nil {
+		log.Fatal(e)
+	}
+	for k, route := range ymlCfg {
+		log.Println(k, route)
+		http.HandleFunc(k, func(w http.ResponseWriter, r *http.Request) {
+			v, ok := ymlCfg[r.URL.String()]
+			if !ok {
+				w.WriteHeader(404)
+				return
+			}
+			log.Println("Requested URL:", r.URL.String(), v)
+			if v.Delay > 0 {
+				<-time.After(v.Delay)
+			}
+			if v.Status != 0 {
+				w.WriteHeader(v.Status)
+			}
+			if v.Body != "" {
+				w.Write([]byte(v.Body))
+			}
+		})
+	}
+	http.ListenAndServe(*addr, nil)
+}
